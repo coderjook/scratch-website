388b3d7604f58df21108071b2e1a1ed39470cca0
Marianne van Roosmalen <jook@jook.nl>
2021-11-11 16:01:39 +0100

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import Home from "./components/Home"
 import Nav from './components/Nav';
 import Snippets from './components/Snippets';
 import Faq from './components/Faq';
 import TutorialsList from './components/TutorialsList';
 import UploadTutorial from './components/UploadTutorial';
 import './css/style.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
  
 
 
 function App() {
   return (
-    <div className="App">
-    <Nav/>
-    <div className="container">
-      <h1>Coderjook Scratch App</h1>
-    </div>
-    
-    <UploadTutorial />
-    <TutorialsList />
-    <Snippets />
-    <Faq />  
-    </div>
+    <>
+    <Router>
+      <div className="App">
+        <Nav/>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+        <Route path="/uploadtutorials" element={<UploadTutorial />} />
+        <Route path="/tutorials"element={<TutorialsList />} />
+        <Route path="/snippets"element={<Snippets />} />
+        <Route path="/faq"element={<Faq />} />  
+        </Routes>
+      </div>
+    </Router>
+    </>
   );
 }
 
