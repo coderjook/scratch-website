7bc49c9989abf354c03ce638c5ed4d98d4c5625a
jonatan verborg <jonatanverborg@gmail.com>
2021-11-28 13:35:52 +0100

    added poc button

--- src/components/storage/StorageList.js
+++ src/components/storage/StorageList.js
@@ -8,47 +8,54 @@ export default function StorageList() {
 const [allItems, setAllItems] = useState([]);
 const [categorie, setCategorie] = useState('');
 
-useEffect(() => {
+useEffect(() => {console.log('all items state', allItems[0])}, [allItems])
+    useEffect(() => {console.log('all items state', allItems)}, [allItems])
+
+
+    useEffect(() => {
   // setAllItems([]);
   getFromFirebase();
-  }, [categorie]) 
+  }, [categorie])
 
-  const getFromFirebase = () => {
+    let currentItems = [];
+console.log(currentItems, 'current items')
 
-      let currentItems = [];
+    const getFromFirebase = () => {
     //1.
     console.log('categorie:', categorie)
     if (categorie) {
         let storageRef = storage.ref().child(categorie);
-      
+
         //2.
         storageRef.listAll().then(function (res) {
             //3.
-            res.items.map((imageRef) => {
-         
+            console.log(res.items, 'res items')
+                res.items.forEach((imageRef) => {
+
             imageRef.getDownloadURL().then((url) => {
                 //4.
                 let currentItem = {
-                    itemUrl: url, 
-                    itemName: imageRef.name 
+                    itemUrl: url,
+                    itemName: imageRef.name
                 }
+                // return currentItem;
+                // console.log(currentItem, 'currentitem')
                 currentItems.push(currentItem);
-                console.log('currentItems:' ,currentItems) 
+                // console.log('currentItems:' ,currentItems)
             });
-            
             });
+            // console.log(newItems, 'new items')
+
         })
         .catch(function (error) {
             console.log(error);
         });
-
-        setAllItems(currentItems)
     }
   };
-
-   const consoleLog = () => {
-        console.log ('consolelog allItems', allItems)
-  }
+  //
+  //  const consoleLog = () => {
+  //       console.log ('consolelog allItems', allItems)
+  // }
 
     const deleteFromFirebase = (url) => {
     //1.
@@ -65,13 +72,19 @@ useEffect(() => {
       });
   };
 
+    const proofOfConcept = () => {
+        setAllItems(currentItems)
+    }
+
     return (
         <div>
             <div className="storagelist">
 
             <div onClick={() => setCategorie('snippets/')} className="btn">bekijk jpg scripts</div>
             <div onClick={() => setCategorie('gif/')} className="btn">bekijk animated gif</div>
-            <div onClick={consoleLog} className="btn">console log</div>
+            <div className="btn">console log</div>
+                {/*{allItems.map((item) => <p>test</p>)}*/}
+                <button onClick={proofOfConcept}>proof of concept</button>
      {allItems.map((item, index) => {
          console.log('item.itemName: ',item.itemName)
         return (
